5c01ae22b0fa26124de3fd6e3bfbf88b3ca9fef1
Mavludaxon <frontendmavludaxon01@gmail.com>
2023-08-15 12:44:43 +0500

    Change desig

--- src/components/Seniority/Seniority.jsx
+++ src/components/Seniority/Seniority.jsx
@@ -1,37 +1,56 @@
-import React from 'react'
-import "./Seniority.scss"
+import "./Seniority.scss";
 
 function Seniority() {
   return (
-    <div className='seniority' >
-      
-        <h1>About me</h1>
-        <div className="history__about">
-          <div className="year__history">
-            <div className="num_year">
-              <span>2020</span>
-              <span>2021</span>
-              <span>2022</span>
-              <span>2023</span>
-            </div>
-            <div className="line_year">
-              <div className="box_year"><div className="in__box"></div></div>
-              <div className="box_year"><div className="in__box"></div></div>
-              <div className="box_year"><div className="in__box"></div></div>
-              <div className="box_year"><div className="in__box"></div></div>
-              <i><div></div></i>
-            </div>
+    <div className="seniority">
+      <h1 className="Aboutme_text">About me</h1>
+      <div className="history__about">
+        <div className="year__history">
+          <div className="num_year">
+            <span>2020</span>
+            <span>2021</span>
+            <span>2022</span>
+            <span>2023</span>
           </div>
-          <div className="texts__history">
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
-            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+          <div className="line_year">
+            <div className="box_year">
+              <div className="in__box"></div>
+            </div>
+            <div className="box_year">
+              <div className="in__box"></div>
+            </div>
+            <div className="box_year">
+              <div className="in__box"></div>
+            </div>
+            <div className="box_year">
+              <div className="in__box"></div>
+            </div>
+            <i>
+              <div></div>
+            </i>
           </div>
         </div>
-     
+        <div className="texts__history">
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Seniority
+export default Seniority;
